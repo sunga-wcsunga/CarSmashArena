49c525d36c4a14565ad2552de2d4fd85bc254dbd
sunga-wcsunga <20220026735@my.xu.edu.ph>
2026-05-31 16:21:50 +0800

    adjusted audio volume

--- js/audio.js
+++ js/audio.js
@@ -2,9 +2,9 @@ const bgm = new Audio("assets/sounds/bgm.mp3");
 
 bgm.loop = true;
 
-bgm.volume = 0.0;
+bgm.volume = 4.0;
 
 const crashSound =
     new Audio("assets/sounds/crash.mp3");
 
-crashSound.volume = 0.0;
+crashSound.volume = 8.0;
