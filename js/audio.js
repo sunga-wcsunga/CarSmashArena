14a206a85c9982b114edd78b4b948ef097b65109
Wencie Sunga <20220026735@my.xu.edu.ph>
2026-05-31 17:05:26 +0800

    try

--- js/audio.js
+++ js/audio.js
@@ -3,15 +3,6 @@ const crashSound = new Audio("assets/sounds/crash.mp3");
 
 bgm.loop = true;
 
-<<<<<<< HEAD
-bgm.volume = 4.0;
-
-const crashSound =
-    new Audio("assets/sounds/crash.mp3");
-
-crashSound.volume = 8.0;
-=======
 bgm.volume = 0.4;
 
 crashSound.volume = 0.8;
->>>>>>> 7b7d28d (Fixed audio loading and collision crash issues)
